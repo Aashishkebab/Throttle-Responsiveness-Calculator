b838f659501af0fb096915133c06ddaca6340fc2
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2026-04-07 22:31:53 -0400

    Initial

---
+++ Torque calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Torque_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] accelerator = File.ReadAllLines("accelerator.csv");
+            string[] throttle = File.ReadAllLines("throttle.csv");
+            string[] boost = File.ReadAllLines("boost.csv");
+            string[] finalCalculation = new string[accelerator.Length];
+
+            for(int i = 0; i < accelerator.Length; i++){
+
+            }
+        }
+    }
+}
